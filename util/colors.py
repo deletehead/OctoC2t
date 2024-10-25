6aa754be04fa4e504f2922f35696d8f85b7846d1
Jake Mayhew <49874223+deletehead@users.noreply.github.com>
2024-10-25 17:20:33 -0400

    Create colors.py

---
+++ util/colors.py
@@ -0,0 +1,15 @@
+# Text Colors
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+
+# Bold and Underline
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+# Reset Color and Style
+RESET = '\033[0m'
